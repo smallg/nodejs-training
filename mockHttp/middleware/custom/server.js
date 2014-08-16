cdd458565b14516be608b0820c586d14bf93894b
smallg <gwpxjb@163.com>
2014-08-16 14:43:39 +0800

    add middleware

---
+++ mockHttp/middleware/custom/server.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var middleware = require('./middleware');
+var app = express();
+app.use('/static',middleware.setHeader());
+app.get('/*', function(req,res) {
+    if(res.head){
+        res.writeHead(res.statusCode,res.header);
+        res.write(res.head);
+        res.write('Hello Test');
+    }
+    res.end();
+});
+app.listen(1337, "127.0.0.1");
---
+++ mockHttp/middleware/custom1/server.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var middleware = require('./middleware');
+var app = express();
+app.get('/',middleware.setHeader(),function(req,res) {
+    res.writeHead(res.statusCode,res.header);
+    res.write(res.head);
+    res.end('Test');
+});
+app.listen(1337, "127.0.0.1");
