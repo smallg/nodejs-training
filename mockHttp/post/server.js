593c160e7ef0050d50a7d5bf2df86b371be98a4c
smallg <gwpxjb@163.com>
2014-08-16 14:12:48 +0800

    add mock example

---
+++ mockHttp/post/server.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var fs = require('fs');
+var querystring = require('querystring');
+var app = express();
+
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/index.html', function (req, res) {
+    req.on('data', function (data) {
+        console.dir(data, data.toString());
+        var obj = querystring.parse(data.toString());
+        console.dir(obj);
+        res.send(obj);
+    });
+});
+app.listen(1337, "127.0.0.1");
